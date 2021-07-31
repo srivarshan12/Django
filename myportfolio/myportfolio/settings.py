cdd4093af0dce6cb4262015ca8580ce97004f1ca
srivarshan12 <varshan1206@gmail.com>
2021-07-31 20:34:27 +0530

    updated

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '=ld9j-adz(^p@p#82$6xl4cg67oko^6o+29fy@i&ypq=3olblz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://srivarshan.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
