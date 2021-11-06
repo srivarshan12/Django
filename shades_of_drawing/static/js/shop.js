48bb5745d16769254802e53a794b8827e22e6b9a
srivarshan12 <varshan1206@gmail.com>
2021-11-06 22:46:57 +0530

    updated

--- shades_of_drawing/static/js/shop.js
+++ shades_of_drawing/static/js/shop.js
@@ -1,9 +1,15 @@
-var $grid = $('.grid').isotope({    
+var $grid = $('.grid').isotope( {
     itemSelector: '.container-item',
     masonry: {
-    columnWidth: 50,
-    gutter: 40,
+        columnWidth: 50,
+        gutter: 40,
         transitionDuration:1500,
         fitWidth:true  
     }  
+
 });
+
+$grid.imagesLoaded().progress( function() {
+    $grid.isotope('layout');
+  });
+
